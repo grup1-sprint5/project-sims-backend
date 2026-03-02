3a8d6cc5be0af34ccce090d029e4abf0a42215f1
jordiarnau05 <jordiarnau@iesmontsia.org>
2026-03-02 16:08:06 +0100

    spatie rols

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,19 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Spatie\Permission\PermissionRegistrar;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    /**
+     * Neteja la caché de permisos de Spatie abans de cada test.
+     * Necessari quan s'usa RefreshDatabase per evitar que els permisos
+     * de la iteració anterior quedin en memòria i provoquin errors.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->app->make(PermissionRegistrar::class)->forgetCachedPermissions();
+    }
 }
