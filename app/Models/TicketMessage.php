45b1f0f4dd477a6eabeb365426b4905e341c53ba
jordiarnau05 <jordiarnau@iesmontsia.org>
2026-03-24 18:45:39 +0100

    Add tenant_id column to ticket_messages table for tenant association

--- app/Models/TicketMessage.php
+++ app/Models/TicketMessage.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Traits\BelongsToTenant;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TicketMessage extends Model
 {
-    use HasFactory;
+    use HasFactory, BelongsToTenant;
 
     protected $table = 'ticket_messages';
 
